42894029438cecbc2548f5fe6015c3a39ab3c328
Joshua Sledge <js038308@broadcom.net>
2020-07-16 11:08:15 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.  Hi There. See the change! Joshua
+          Edit <code>src/App.js</code> and save to reload.  Hi There. See the change! Joshua.  Changed on the feature branch
         </p>
         <a
           className="App-link"
